18efae3ab23e5212a8ed6eb41515a64df2ad0bd9
muizmalik99 <muizmalik100@gmail.com>
2025-12-10 23:09:01 +0500

    api-integration merge 1

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL!;
+const baseURL = import.meta.env.VITE_API_BASE_URL!; 
 
-export const api = axios.create({
-  baseURL,
-});
+export const api = axios.create({ baseURL });
 
 api.interceptors.request.use(
   (config) => {
