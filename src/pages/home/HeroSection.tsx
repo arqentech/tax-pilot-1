51d23d5a5253836383f09eaeabd59daee2b5ed90
muizmalik99 <muizmalik100@gmail.com>
2026-01-15 12:52:03 +0500

    Hero image fix

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -5,19 +5,17 @@ import { floatUpDown } from "@/animations/float";
 
 const HeroSection = () => {
   return (
-    <section className="full-bleed relative bg-[#FBFBFA] py-12 md:py-15">
-      <div className="global-container flex flex-col gap-8 md:flex-row md:items-center md:justify-between md:gap-16">
-        <div className="w-full flex flex-col items-center text-center md:items-start md:text-left space-y-6 md:space-y-8">
-          <h1 className="font-bricolage main-heading max-w-full">
-            <span className="block whitespace-nowrap">
-              Online taxes made
-            </span>
+    <section className="full-bleed relative bg-[#FBFBFA] ">
+      <div className="global-container grid md:grid-cols-2 gap-2 md:gap-16 items-center py-10">
+        <div className="flex flex-col items-center text-center md:items-start md:text-left space-y-6 py-10">
+          <h1 className="font-bricolage main-heading max-w-full -mt-10">
+            <span className="block whitespace-nowrap">Online taxes made</span>
             <span className="block whitespace-nowrap">
               Simple with Tax Pilot.
             </span>
           </h1>
 
-          <p className="text-base leading-relaxed w-full opacity-90">
+          <p className="text-base leading-relaxed w-full">
             Access over 150 certified CAF and patronage services, guided step by
             step by real experts, all from the comfort of your home.
           </p>
@@ -27,34 +25,32 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        <div className="mx-auto w-full items-center justify-center max-w-[360px] sm:max-w-[460px] md:max-w-[600px] lg:max-w-[646px] md:mx-0 hero-image-container">
-          <div className="relative w-full max-w-[800px] mx-auto">
-            <img
-              src="/svg/home/hero-illustration-1.svg"
-              alt="Man using laptop for tax calculation"
-              className="h-auto w-full object-contain relative top-18 pt-10  md:-left-24 md:top-19"
+        <div className="relative w-full max-w-[700px] mx-auto">
+          <img
+            src="/svg/home/hero-illustration-1.svg"
+            alt="Man using laptop for tax calculation"
+            className="w-full h-[clamp(300px,60vw,500px)] object-contain mt-10"
+            loading="lazy"
+          />
+
+          <div className="absolute md:top-[5%] md:right-[-3%] sm:top-[5%] sm:right-[15%] top-[5%] right-[7%] flex items-start gap-2">
+            <motion.img
+              src="/svg/home/hero-illustration-3.svg"
+              alt="Dollar icon"
+              className="w-[clamp(32px,5vw,60px)] h-auto object-contain"
               loading="lazy"
+              variants={floatUpDown(12, 3)}
+              animate="animate"
             />
 
-            <div className="absolute right-0 top-0 flex items-start gap-2">
-              <motion.img
-                src="/svg/home/hero-illustration-3.svg"
-                alt="Dollar icon"
-                className="w-[clamp(32px,8vw,60px)] h-auto object-contain"
-                loading="lazy"
-                variants={floatUpDown(12, 3)}
-                animate="animate"
-              />
-
-              <motion.img
-                src="/svg/home/hero-illustration-2.svg"
-                alt="Percentage icon"
-                className="w-[clamp(38px,10vw,80px)] h-auto object-contain mt-4"
-                loading="lazy"
-                variants={floatUpDown(16, 3.5, 0.4)}
-                animate="animate"
-              />
-            </div>
+            <motion.img
+              src="/svg/home/hero-illustration-2.svg"
+              alt="Percentage icon"
+              className="w-[clamp(38px,8vw,80px)] h-auto object-contain mt-4"
+              loading="lazy"
+              variants={floatUpDown(16, 3.5, 0.4)}
+              animate="animate"
+            />
           </div>
         </div>
       </div>
